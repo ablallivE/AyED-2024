a470270cfb9f742401e5248d6d0527d247d8e654
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-18 19:53:50 -0300

    subo 7 8

---
+++ Etapa1/7_Villalba_AlReves/7_Villalba_AlReves/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Villalba_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("ingresar 3 letras");
+            string letra1;
+            letra1 = Console.ReadLine();
+            string letra2;
+            letra2 = Console.ReadLine();
+            string letra3;
+            letra3 = Console.ReadLine();
+
+            Console.WriteLine("este es el orden inicial "+letra1+ letra2+ letra3);
+            Console.WriteLine("al revez "+letra3+ letra2+ letra1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa1/8_Villalba_EsMayor/8_Villalba_EsMayor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Villalba_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su edad");
+            int edad;
+            edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("tu edad es" + edad);
+            if(edad < 18)
+            {
+                Console.WriteLine("sos menor");
+            }
+            else if(edad > 18)
+            {
+                Console.WriteLine("sos mayor de edad");
+            }
+            Console.ReadKey();
+        }
+    }
+}
