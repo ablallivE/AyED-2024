f567af4cb93677746fae177d794a6891b16f42f6
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-18 17:00:53 -0300

    subo tp 9

---
+++ Etapa1/9_Villalba_IngresarPassword/9_Villalba_IngresarPassword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Villalba_IngresePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string claveR;
+            claveR = "enzo1234";
+            string claveP;
+            Console.WriteLine("ingrese la contraseña");
+            claveP = Console.ReadLine();
+
+            if (claveP == claveR)
+            {
+                Console.WriteLine("es correcta");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
