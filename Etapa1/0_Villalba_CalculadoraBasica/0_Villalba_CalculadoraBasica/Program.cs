ad94c0c169a21e83fd7d3aa17062d96177ebdd17
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-18 16:38:23 -0300

    subo tp 0 y 1

---
+++ Etapa1/0_Villalba_CalculadoraBasica/0_Villalba_CalculadoraBasica/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Villalba_CalculadoraBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            Console.WriteLine("escribir el primer numero");
+            numero1 = int.Parse(Console.ReadLine());
+            int numero2;
+            Console.WriteLine("escribir el segundo numero");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("la suma es " + (numero1 + numero2));
+            Console.WriteLine("la resta es " + (numero1 - numero2));
+            Console.WriteLine("la multiplicacion es " + (numero1 * numero2));
+            Console.WriteLine("la division es " + (numero1 / numero2));
+            Console.WriteLine("el resto es " + (numero1 % numero2));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa1/1_Villalba_LaSumaAnterior/1_Villalba_LaSumaAnterior/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Villalba_LaSumaAnterior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            Console.WriteLine("pon un numero");
+            numero1 = int.Parse(Console.ReadLine());//leer un numero
+            int numero2;
+            Console.WriteLine("pon un numero");
+            numero2 = int.Parse(Console.ReadLine());//leer un numero
+            int numero3;
+            Console.WriteLine("pon un numero");
+            numero3 = int.Parse(Console.ReadLine());//leer un numero
+            int numero4;
+            numero4 = (numero1 + numero2 + numero3);
+            Console.WriteLine("el numero4 es " + (numero4));
+
+            Console.ReadKey();
+        }
+    }
+}
