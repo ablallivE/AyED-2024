1050312d28687ceb892af0127af6c0f4ddd9fefa
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-18 17:07:37 -0300

    subo tp 2 y 3

---
+++ Etapa1/2_Villalba_BienvenidoACiudad/2_Villalba_BienvenidoACiudad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Villalba_BienvenidoACiudad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escribir un nombre y una ciudad");
+            string nombre;
+            nombre = Console.ReadLine();
+            string ciudad;
+            ciudad = Console.ReadLine();
+            Console.WriteLine("Hola "+ nombre + " bienvenido a " + ciudad);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa1/3_Villalba_DimensionesDeEsfera/3_Villalba_DimensionesDeEsfera/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Villalba_DimensionesDeEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radio;
+            Console.WriteLine("ingresar el radio");
+            radio = int.Parse(Console.ReadLine());
+            Console.WriteLine("la superficie de la esfera es " + (4 * 3.14 * (radio^2)));
+            Console.WriteLine("el volumen de la esfera es " + (4 * 3.14)/3 * (radio^3));
+
+            Console.ReadKey();
+        }
+    }
+}
