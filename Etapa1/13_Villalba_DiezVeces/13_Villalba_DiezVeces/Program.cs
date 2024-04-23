01107ef562ee0846542b4b55064786763af1fad8
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-23 16:28:09 -0300

    subo 13

---
+++ Etapa1/13_Villalba_DiezVeces/13_Villalba_DiezVeces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Villalba_DiezVeces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese una palabra");
+            String palabra;
+            palabra = Console.ReadLine();
+            int contador;
+            contador = 1;
+
+            while(contador < 10)
+            {
+                Console.WriteLine(palabra);
+                contador++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
