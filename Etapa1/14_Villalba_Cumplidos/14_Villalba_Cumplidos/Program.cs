957d8151155d48609f2ee686bf763a285ca92261
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-23 16:51:13 -0300

    subo 14 15

---
+++ Etapa1/14_Villalba_Cumplidos/14_Villalba_Cumplidos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Villalba_Cumplidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su edad");
+            int edad;
+            edad = int.Parse(Console.ReadLine());
+
+            int contador;
+            contador = 1;
+
+            Console.WriteLine("estos son los años que cumpliste");
+
+            while (contador <=edad)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa1/15_Villalba_CuentaRegresiva/15_Villalba_CuentaRegresiva/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Villalba_CuentaRegresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero");
+
+            int numero;
+            numero = int.Parse(Console.ReadLine());
+
+            int contador;
+            contador = numero;
+
+            Console.WriteLine("cuenta regresiva");
+
+            while (contador >= 0)
+            {
+                Console.WriteLine(contador);
+                contador--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
