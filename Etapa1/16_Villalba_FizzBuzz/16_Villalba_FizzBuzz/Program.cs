3593ae9faf415fbfd626775c4d087c7874aa602a
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-25 14:43:02 -0300

    subo 16

---
+++ Etapa1/16_Villalba_FizzBuzz/16_Villalba_FizzBuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Villalba_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = 0;
+            while (num < 100)
+            {
+                num++;
+
+                if (num % 3 != 0 && num % 5 != 0)
+                {
+                    Console.WriteLine(num);
+                }
+ 
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+
+                if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+            }              
+                Console.ReadKey();
+
+
+           
+            }
+        }
+    }
