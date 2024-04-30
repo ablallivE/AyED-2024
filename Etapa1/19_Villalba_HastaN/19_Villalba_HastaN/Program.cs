fe1664c67af2575f9e2f51d2600bc5896f7c91fc
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-30 14:58:44 -0300

    subo 19

---
+++ Etapa1/19_Villalba_HastaN/19_Villalba_HastaN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Villalba_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("ingresar valor N");
+            N = int.Parse(Console.ReadLine());
+
+            int numero;
+            numero = 1;
+
+            while (numero < N)
+            {
+                numero++;
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
