b99e78bce577583a5119443b7e725f534b867f74
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-04-30 14:22:18 -0300

    subo 18 tp :P

---
+++ Etapa1/18_Villalba_HastaQueSI/18_Villalba_HastaQueSI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Villalba_HastaQueSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta = " ";
+
+            Console.WriteLine("DIGA SI PARA CONTINUAR");
+
+            while (respuesta != "SI")
+            {
+                Console.WriteLine("¿desea terminar el programa?");
+                respuesta = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("¡Hasta la vista!");
+            Console.ReadKey();
+
+        }
+    }
+}
