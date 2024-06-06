0a50d1ecdb2b57280073681f48caea1fc7da47f2
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-06-06 16:49:57 -0300

    tp 7

---
+++ Etapa2/7_villalba_ContratacionSoft/7_villalba_ContratacionSoft/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_villalba_ContratacionSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            Console.WriteLine("¿cuantos candidatos hay?");
+            int[] vector = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < vector.Count(); i++)
+            {
+                Console.WriteLine("Participantes " + (i + 1));
+                Console.WriteLine("¿cual es su puntaje   ?");
+                int respuesta = aleatorio.Next(100, 400);
+
+                vector[i] = respuesta;
+                Console.WriteLine(vector[i]);
+
+            }
+            Console.WriteLine("-Orden-");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = 0; j < vector.Length - 1; j++)
+                {
+                    if (vector[j] > vector[j + 1])
+                    {
+                        int temp = vector[j];
+                        vector[j] = vector[j + 1];
+                        vector[j + 1] = temp;
+
+                    }
+
+                }
+               
+            }
+            for (int i = 0; i < vector.Count(); i++)
+            {
+                Console.WriteLine(vector[i] + " puntos");
+
+            }
+
+            Console.WriteLine("escriba el filtro");
+            int filtro = int.Parse(Console.ReadLine());
+            Console.WriteLine("*estos son los puntajes multiplos del filtro *");
+
+            for (int i = 0; i < vector.Count(); i++)
+            {
+                if (vector[i] % filtro == 0 )
+                {
+                    Console.WriteLine("° Puntaje numero " + (i + 1) + " con valor " + vector[i] + " °");
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
