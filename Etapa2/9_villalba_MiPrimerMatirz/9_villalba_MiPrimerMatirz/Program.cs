a6f454241bb8fa06d0cc65c1df3349fb76b14501
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-07-02 16:28:10 -0300

    subo tp 9

---
+++ Etapa2/9_villalba_MiPrimerMatirz/9_villalba_MiPrimerMatirz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_villalba_MiPrimerMatirz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el el valor de N y M");
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[N, M];
+            for (int c = 0; c < M; c++)
+            {
+                
+                for (int f = 0; f < N; f++)
+                {
+                    matriz[f, c] = 0;
+                    Console.Write(matriz[f , c]);
+                   
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
