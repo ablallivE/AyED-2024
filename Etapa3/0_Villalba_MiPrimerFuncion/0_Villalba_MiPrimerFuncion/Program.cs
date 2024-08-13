457f96a7ad0ec5eddfdb5094a6a6205920abb9e9
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-08-13 14:52:58 -0300

    tp 0

---
+++ Etapa3/0_Villalba_MiPrimerFuncion/0_Villalba_MiPrimerFuncion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Villalba_MiPrimerFuncion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("¿cual es tu nombre?");
+            M(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+        }
+        static string M(string nombre)
+        {
+            Console.WriteLine("¡Hola " + nombre + " !");
+            return nombre;
+
+        }
+    }
+}
