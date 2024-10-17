75f2589e3355ef2901124ed05d46186e9fb4094e
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-10-17 16:56:52 -0300

    tp 1

---
+++ Etapa4/1_villalba_CobrarBecaGUI/1_villalba_CobrarBecaGUI/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_villalba_CobrarBecaGUI
+{
+    public partial class monto : Form
+    {
+        public monto()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox1.Text) >= 19)  
+            {
+                if (ingresos.Text == "100,001 - 200,000" || ingresos.Text == "Más de 200,000")
+                {
+                    MessageBox.Show("la beca ha sido aceptada");
+                }
+                else
+                {
+                    MessageBox.Show("la beca ha sido rechazada\n ingresos invalidos");
+                }
+            }
+            else
+            {
+                MessageBox.Show("la beca ha sido rechazada\n edad invalida");
+            }
+        }
+    }
+}
