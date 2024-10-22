ad7e96cedf94de41bccfadd115b5e5b46afab69d
ablallivE <167466950+ablallivE@users.noreply.github.com>
2024-10-22 16:05:00 -0300

    tp 2

---
+++ Etapa4/2_villalba_Loginn/2_villalba_Loginn/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace _2_villalba_Loginn
+{
+    partial class Fa
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.contra = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.ver = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(180, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FEISBU\'";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(183, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "nombre de usuario";
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(187, 155);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(183, 20);
+            this.usuario.TabIndex = 2;
+            // 
+            // contra
+            // 
+            this.contra.Location = new System.Drawing.Point(187, 215);
+            this.contra.Name = "contra";
+            this.contra.PasswordChar = '*';
+            this.contra.Size = new System.Drawing.Size(183, 20);
+            this.contra.TabIndex = 3;
+            this.contra.TextChanged += new System.EventHandler(this.contra_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(183, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "contraseña";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(201, 269);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(150, 64);
+            this.login.TabIndex = 5;
+            this.login.Text = "LOGIN";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ver
+            // 
+            this.ver.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ver.Location = new System.Drawing.Point(376, 212);
+            this.ver.Name = "ver";
+            this.ver.Size = new System.Drawing.Size(34, 23);
+            this.ver.TabIndex = 6;
+            this.ver.Text = "ver ";
+            this.ver.UseVisualStyleBackColor = true;
+            this.ver.Click += new System.EventHandler(this.create_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_2_villalba_Loginn.Properties.Resources.descarga;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Fa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(590, 492);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ver);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.contra);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Fa";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Fa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.TextBox contra;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button ver;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Etapa4/2_villalba_Loginn/2_villalba_Loginn/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_villalba_Loginn
+{
+    public partial class Fa : Form
+    {
+        public Fa()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (usuario.Text == "Enzo2008")
+            {
+                if (contra.Text == "30enzo30")
+                {
+                    MessageBox.Show("ingreso permitido");
+                }
+                else
+                {
+                    MessageBox.Show("usuario o contraseña invalida");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("usuario o contraseña invalida");
+            }
+        }
+
+        private void contra_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(contra.Text);
+        }
+
+        private void Fa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
